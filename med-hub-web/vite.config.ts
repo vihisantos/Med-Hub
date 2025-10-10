e22f848464faf9bd71d7656b76c2ae47ec8ec100
Vitor Santos <128942186+vihisantos@users.noreply.github.com>
2025-10-10 13:05:16 +0000

    att

--- med-hub-web/vite.config.ts
+++ med-hub-web/vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js']
-  }
+  plugins: [react()]
 })
